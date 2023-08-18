0a76fdb64e1563ff6bde8a21e8e50ca6a14e981f
laughing0900@gmail.com <laughing0900@gmail.com>
2023-08-18 10:40:52 +0800

    feat: video placeholder

--- ai_chat/src/components/chat/BotAvatar.tsx
+++ ai_chat/src/components/chat/BotAvatar.tsx
@@ -1,7 +1,11 @@
 export const BotAvatar: React.FC = () => {
   return (
-    <div className="w-full h-full flex justify-center mb-20">
-      <div className="h-full aspect-square rounded-full bg-blue-300"></div>
+    <div className="mb-20 flex h-full w-full justify-center">
+      <div className="flex aspect-square h-full items-center overflow-hidden rounded-full bg-blue-300">
+        <video autoPlay className="h-full w-full object-cover" loop>
+          <source src="placeholder.mp4" type="video/mp4" />
+        </video>
+      </div>
     </div>
   );
 };
