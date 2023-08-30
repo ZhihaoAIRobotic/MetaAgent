c7d572fbbd50fa94a82fe6b61e86b4393dbab519
Zhihao <1155187971@link.cuhk.edu.hk>
2023-08-31 01:51:50 +0800

    Update the method of loading config

--- dima/app.py
+++ dima/app.py
@@ -6,17 +6,20 @@ from docarray.documents import TextDoc
 from docarray import DocList
 from dima.models import Chatglm2
 from memory.processing import Dataloader
+import json
+import yaml
 
-
+with open('./dima/config.yaml', 'r') as config_file:
+    config = yaml.safe_load(config_file)
 
 
 def get_class(class_name):
     # 👇️ returns None if class with given name doesn't exist
     return getattr(sys.modules[__name__], class_name, None)
 
- 
 
-f = Flow().add(name='myexec1', uses=Chatglm2)
+# Chatglm2 = get_class('Chatglm2')
+f = Flow().add(name='LLM_Model', uses=get_class(config['LLM_Model']))
 # dep = Deployment(uses=ChatGLM2, port=60008, protocol='http')
 
 
